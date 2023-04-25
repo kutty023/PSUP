deaa7fd234f797e062d5256d0bc935f787683f70
ARTHI R <arthiaaru23@gmail.com>
2023-04-25 17:23:52 +0530

    Renamed

--- pr1.py
+++
@@ -1,8 +0,0 @@
-# Python program to extract 2nd last digit
-
-n = int(input()) #Getting input from the user
-if(n>=10):          #checking if the input contains more than one digit
-    s = int(n/10)%10    #n is divided by 10 to remove the last digit and then gets the remainder using modulus
-    print(s)
-else:
-    print("Invalid number") #if n is lessthan 10 prints invalid
